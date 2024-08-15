914230e82c342ded2a246583f305a9691819fe08
Ubuntu <ubuntu@ip-172-31-35-109.ec2.internal>
2024-08-15 01:46:59 +0000

    log script

---
+++ script_404.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Create a BASH script that will search the log file for "404" errors 
+#identify the IP addresses associated with them. 
+# Then provide the number of times each IP encountered a 404.
+#get 404 errors
+
+
+grep " 404 " web-server-access-logs.log > 404_logs.log
+#example: 3089189,"5.210.140.170 - - [23/Jan/2019:12:43:40 +0330] ""GET /apple-touch-icon-120x120.png HTTP/1.1"" 404 33679 ""-"" ""MobileSafari/604.>
+#get the ip address
+#do the awk twice use, 
+awk -F'\"' '{print $2}' 404_logs.log | awk '{print $1}' > 404_ip_logs_temp.log 
+sort 404_ip_logs_temp.log | uniq -c | sort -r > 404_ip_logs.log
+rm 404_ip_logs_temp.log
+echo "Done"
